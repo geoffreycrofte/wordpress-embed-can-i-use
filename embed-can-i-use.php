dca6fbcc40fbbf7ea2b1dbf83fd9adb1bc0a1c60
Geoffrey Crofte <support@creativejuiz.com>
2016-05-25 12:29:55 +0200

    First release

---
+++ embed-can-i-use.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: Embed Can I Use
+Description: Add Can I Use support tables to your WordPress web site thanks to this shortcode. <code>[ ciu_embed ]</code>
+Author: Geoffrey Crofte
+Version: 1.0.0
+Author URI: http://geoffrey.crofte.fr
+License: GPLv2 or later
+Text domain: embed-can-i-use
+Domain Path: /languages
+
+
+
+Copyright 2016  Geoffrey Crofte  (email : support@creativejuiz.com)
+Embedded makes possible thanks to the work done at http://caniuse.bitsofco.de/
+
+    
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+define( 'ECIU_SCRIPT_VERSION', '1.0.1' );
+	
+/**
+ * Makes this plugin translatable
+ *
+ * @since  1.0.0
+ * @author Geoffrey
+ */
+add_action( 'init', 'ciue_multilang' );
+function ciue_multilang() {
+	load_plugin_textdomain( 'embed-can-i-use', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+
+/**
+ * New ciu_embed shortcode
+ *
+ * @since  1.0.0
+ * @author Geoffrey
+ */
+add_shortcode( 'ciu_embed', 'add_ciue_shortcode' );
+function add_ciue_shortcode( $attr ) {
+
+	extract( shortcode_atts( array(
+		'feature' 	=> '',
+		'periods' 	=> 'past_1,current,future_1',
+	), $attr ) );
+
+	$return = current_user_can( 'remove_users' ) ? sprintf( esc_html__( 'You need to complete %sfeature%s shortcode attribute', 'embed-can-i-use' ), '<code>', '</code>' ) :  '';
+
+	// if no feature filled in
+	if ( $feature === '' ) {
+		return $return;
+	}
+
+	$periods = preg_replace(
+		array( '#\-([1-9]{1})#', '#\+([1-9]{1})#'),
+		array( 'past_$1', 'future_$1' ),
+		$periods
+	);
+
+	if ( ! preg_match( '#current#', $periods ) ) {
+		$periods .= 'current';
+	}
+
+	$link_text = apply_filters( 'ciue_link_text', '<p>Can I Use ' . $feature . '?</p>', $feature );
+	$link_attr = apply_filters( 'ciue_link_target', ' target="_blank"' );
+
+	$cui = '<div class="ciu_embed" data-feature="' . $feature . '" data-periods="' . $periods . '"><a href="http://caniuse.com/#feat=' . $feature . '"' . $link_attr . '>' . $link_text . '</a></div>';
+
+	wp_enqueue_script( 'can_i_use_embedded', '//cdn.jsdelivr.net/caniuse-embed/' . ECIU_SCRIPT_VERSION . '/caniuse-embed.min.js', array(), ECIU_SCRIPT_VERSION, true );
+
+	return $cui;
+
+}
